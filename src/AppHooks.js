63594542a58e3d22e7e6b3f2365f664ea4d68cf4
danlagos <lagos.d@gmail.com>
2019-06-19 23:49:30 -0700

    switched to react without using hooks.

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -6,28 +6,26 @@ import { listNotes } from './graphql/queries';
 import { onCreateNote, onDeleteNote, onUpdateNote } from './graphql/subscriptions';
 
 const App = () => {
-  // creating a "state" with react hooks format
+  // creating a "state" using react hooks format
   const [id, setId] = useState("")
   const [note, setNote] = useState("")
   const [notes, setNotes] = useState([])
 
   useEffect(() => {
     getNotes();
-    const createNoteListener = API.graphql(
-      graphqlOperation(onCreateNote)
-      ).subscribe({
+    const createNoteListener = API.graphql(graphqlOperation(onCreateNote)).subscribe({
       next: noteData => {
         const newNote = noteData.value.data.onCreateNote;
         setNotes(prevNotes => {
           const oldNotes = prevNotes.filter(note => note.id !== newNote.id)
-          const updatedNotes = [... oldNotes, newNote]
+          const updatedNotes = [...oldNotes, newNote]
           return updatedNotes
         })
+        setNote("");
       }
     });
 
-    const deleteNoteListener = API.graphql(graphqlOperation(onDeleteNote)).subscribe(
-      {
+    const deleteNoteListener = API.graphql(graphqlOperation(onDeleteNote)).subscribe({
         next: noteData => {
           const deletedNote = noteData.value.data.onDeleteNote
           setNotes(prevNotes => {
@@ -38,8 +36,7 @@ const App = () => {
         }
       });
 
-    const updatedNoteListener = API.graphql(graphqlOperation(onUpdateNote)).subscribe(
-    {
+    const updatedNoteListener = API.graphql(graphqlOperation(onUpdateNote)).subscribe({
       next: noteData => {
         const updatedNote = noteData.value.data.onUpdateNote;
         setNotes(prevNotes => {
@@ -66,9 +63,9 @@ const App = () => {
   const getNotes = async () => {
     const result = await API.graphql(graphqlOperation(listNotes));
     setNotes(result.data.listNotes.items);
-  }
+  };
 
-  const handleChangeNote = event => setNote(event.target.value)
+  const handleChangeNote = event => setNote(event.target.value);
 
   const hasExistingNote = () => {
     if (id) {
@@ -76,7 +73,7 @@ const App = () => {
       return isNote;
     }
     return false;
-  }
+  };
 
   const handleAddNote = async event => {
     event.preventDefault();
@@ -90,7 +87,6 @@ const App = () => {
   };
 
   const handleUpdateNote = async () => {
-
     const input = { id, note };
     await API.graphql(graphqlOperation(updateNote, { input })
     );
@@ -106,6 +102,7 @@ const App = () => {
     setNote(note)
     setId(id)
   }
+
     return (
       <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
         <h1 className="code f2-l">Amplify Notetaker</h1>
@@ -125,9 +122,10 @@ const App = () => {
         </form>
         {/* Notes List*/}
         <div>
-          { notes.map(item => (
-            <div key={ item.id } className="flex items-center">
-              <li onClick={() => handleSetNote(item) } className="list pa1 f3">
+          {notes.map(item => (
+            <div key={item.id} className="flex items-center">
+              <li
+                onClick={() => handleSetNote(item)} className="list pa1 f3">
                 { item.note }
               </li>
               <button
